bedd018c90bacb140a886120cdf393c7e9a7b336
angussidney <angussidney@users.noreply.github.com>
2016-07-14 18:24:05 +1000

    added py file and icon

---
+++ rpg.py
@@ -0,0 +1,53 @@
+import random, pygame, sys
+from pygame.locals import *
+
+# All options for game
+WINDOWWIDTH = 800
+WINDOWHEIGHT = 600
+WINDOWCAPTION = "RPG Game"
+WINDOWICON = "icon.png"
+
+FONTSIZE = 20
+
+
+def main ():
+    global DISPLAY, FPSCLOCK, BASICFONT
+
+    pygame.init()
+
+    # Set up display and icons
+    DISPLAY = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    pygame.display.set_caption(WINDOWCAPTION)
+    icon = pygame.image.load(WINDOWICON)
+    pygame.display.set_icon(icon)
+    
+    # FPS
+    FPSCLOCK = pygame.time.Clock()
+
+    # Fonts
+    BASICFONT = pygame.font.Font('freesansbold.ttf', FONTSIZE)
+    
+    
+    while True: # main game loop
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+        pygame.display.update()
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+class Character(object):
+    scores = {
+        'strength': 10,
+        'dex': 10,
+        'con': 10,
+        'intelligence': 10,
+        'wis': 10,
+        'cha': 10
+    }
+
+
+if __name__ == '__main__':
+    main()
