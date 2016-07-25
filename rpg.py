543ccb0ef5104240ddf2b9dae00f95d7d9ec9b39
angussidney <angussidney@users.noreply.github.com>
2016-07-25 15:09:16 +1000

    finished skills, proficiencies and calculator

--- rpg.py
+++ rpg.py
@@ -49,16 +49,38 @@ class Character(object):
         'cha': 10
     }
     skills = {
-        'athletics': False #str
-        'acrobatics': False #dex
-        'sleight_of_hand': False #dex
-        'stealth': False #dex
-        'arcana': False #int
-        'history': False #int
-        'investigation': False #int
-        'nature': False #int
-        'religion': False #int
+        'athletics': (False, 'strength'),
+        'acrobatics': (False, 'dex'),
+        'sleight_of_hand': (False, 'dex'),
+        'stealth': (False, 'dex'),
+        'arcana': (False, 'intelligence'),
+        'history': (False, 'intelligence'),
+        'investigation': (False, 'intelligence'),
+        'nature': (False, 'intelligence'),
+        'religion': (False, 'intelligence'),
+        'animal_handling': (False, 'wis'),
+        'insight': (False, 'wis'),
+        'medicine': (False, 'wis'),
+        'perception': (False, 'wis'),
+        'survival': (False, 'wis'),
+        'deception': (False, 'cha'),
+        'intimidation': (False, 'cha'),
+        'performance': (False, 'cha'),
+        'persuasion': (False, 'cha')
     }
+    saving_throws = {
+        'strength': False,
+        'dex': False,
+        'con': False,
+        'intelligence': False,
+        'wis': False,
+        'cha': False
+    }
+    def calculate_skill (self, skill):
+        if self.skills[skill][0] == True:
+            return self.scores[skills[skill][1]] + self.proficiency_bonus
+        else:
+            return self.scores[skills[skill][1]]
 
 
 if __name__ == '__main__':
